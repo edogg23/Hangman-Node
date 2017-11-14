93b69674f72b0c0ecebdeaff899b4e95ad65158b
Ernest C <Ernest C>
2017-11-14 18:01:03 -0500

    Tutor jacked up my program trying to get it back to working condition

--- hangman.js
+++ hangman.js
@@ -4,8 +4,8 @@ var inquirer = require('inquirer');
 var rdmWord = require('./wordbank.js');
 var letter = require('./letter.js');
 
-var word = rdmWord.wordChoice(rdmWord.wordBank)
-console.log("word: " , word);
+// var word = rdmWord.wordChoice(rdmWord.wordBank
+console.log("word: " + rdmWord);
 
 
 inquirer.prompt([
@@ -22,6 +22,38 @@ inquirer.prompt([
 			console.log("letter array: ", ltrAry);
 
 			var checkWord = new rdmWord(letter);
+
+			wordLength = wordChoice.length;
+    		guessedWord = [];
+    		guessedLetters = [];
+    		guessesRemaining = 15;
+		    correctGuessedLetters = 0;
+
+		    for (var i = 0; i < wordLength; i++) {
+       		guessedWord.push("_");
+
+       		var matchFound = false;
+
+       		for (var i = 0; i < wordChoice.length; i++){
+                wordChoice.charAt(i);
+                if (wordChoice.charAt(i) == keyPressed) {
+                    guessedWord[i] = keyPressed;
+                    correctGuessedLetters++;
+                    matchFound = true;
+                }
+            }
+
+            if (matchFound && wordChoice.length == correctGuessedLetters) {
+                notification("You Won", elementNotificationWin);
+                gameActive = false;
+            }
+
+            //If they lose
+            if (guessesRemaining == 0){
+                notification("You Lost dude", elementNotificationLose);
+                gameActive = false;
+            }
+    }  
 		});
 
 
--- wordbank.js
+++ wordbank.js
@@ -9,23 +9,24 @@ var wordBank = [
 	"loan"
 ];
 
-module.exports = function Word() {
-	this.randomWord = wordBank[Math.floor(Math.random() * wordBank.length)];
-}
-
-//chooses a randome word from wordBank
-// var wordChoice = function(list) {
-// 	var randomWord = list[Math.floor(Math.random() * list.length)];  
-// 	console.log("wordbank.js: " + randomWord);
+// module.exports = function Word() {
+// 	this.randomWord = wordBank[Math.floor(Math.random() * wordBank.length)];
 // 	return (randomWord);
-// };
+// }
+
+// chooses a randome word from wordBank
+var wordChoice = function(list) {
+	var randomWord = list[Math.floor(Math.random() * list.length)];  
+	console.log("wordbank.js: " + randomWord);
+	return (randomWord);
+};
 
 
 
 
 // console.log(wordChoice());
 
-// module.exports = {
-// 	wordChoice: wordChoice,
-// 	wordBank: wordBank
-// };
+module.exports = {
+	wordChoice: wordChoice,
+	wordBank: wordBank
+};
